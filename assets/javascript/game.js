42c7cdb7459657fa612dc47d7903f5cd8dbf0a09
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-06-23 20:50:13 -0400

    Created card for wins and loses, as well as id's for wins, loses, and player score.

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+//DONT FORGET TO LINK TO PORTFOLIO
+//DONT FORGET READ.ME
+
+//Display game name and instructions to player ---DONE
+
+//Display Random number generated 
+
+//Display number of wins and loses
+
+//Display four crystals as buttons on the page. ---DONE
+
+
+//The random number displayed should be 19-120
+
+//This random number should be changed every turn
+
+
+//Make each crystal worth a specific amount of points
+
+//Make the point value for each chrystal change every time.
+
+//Point values for chrystals are between 1-12
+
+//Add the chosen chrystal value to the total score
+
+//If total score is over random number generated, add one loss to loses
+
+//If total score is equal to random number generated, add one win to wins
+
+//If over or equal to, reset game. New random number generated, new values for each chrystal 
